e6a974718ab45947f28257bf1daa3ffe6a153783
David Sanchez Romero <dsebastiansr@hotmail.com>
2023-08-23 16:02:07 -0500

    [URL]

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chinigumy.github.io/guia-reparacion-laptop'
+  base: '/guia-reparacion-laptop/'
 })
